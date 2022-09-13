6c6f9759170d830ff02ada6b9d8f5c9890b751ad
Monjul Boruah <monjulboruah@gmail.com>
2022-09-13 23:38:02 +0530

    deploy

---
+++ fun.py
@@ -0,0 +1,13 @@
+def sum(a,b):
+    return a+b
+    
+def armstrong(n):
+    n = int(input("Enter the number\n"))
+    
+    if(n > 0):
+        print("less than 0")
+        return True
+    else:
+        print("more than 0")
+        return False
+
